5f34255bb577d491ed34786cbbf5f086bb69ef04
Achintya777 <59294509+Achintya777@users.noreply.github.com>
2020-06-23 17:58:25 +0530

    Create bipartite.cpp

---
+++ Graph_algorithms/bipartite.cpp
@@ -0,0 +1,14 @@
+//biparite graph: if a graph can b divided in 2 sets such that each set shouldn't have any directly connected nodes 
+void dfs(int u,int cu=0){
+  if(~c[u]){
+    if(c[u]^cu){
+      //graph is not bipartite 
+      exit(0) ;
+    }
+    return ;
+  }
+  c[u]=cu ;
+  for(int v:adj[u])
+    dfs(v,cu^1);
+}
+memset(c,-1,sizeof(c)) ;
