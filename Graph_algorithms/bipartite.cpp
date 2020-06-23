d2bf39e56708afd1e618c4c7ae085f6ef94788ca
Achintya777 <59294509+Achintya777@users.noreply.github.com>
2020-06-23 18:10:07 +0530

    Update bipartite.cpp

--- Graph_algorithms/bipartite.cpp
+++ Graph_algorithms/bipartite.cpp
@@ -1,4 +1,5 @@
 //biparite graph: if a graph can b divided in 2 sets such that each set shouldn't have any directly connected nodes 
+int c[mxN] ;
 void dfs(int u,int cu=0){
   if(~c[u]){
     if(c[u]^cu){
