958cc55224c373b341fa0e79960daeb6263d3f5c
Achintya777 <59294509+Achintya777@users.noreply.github.com>
2020-07-10 17:14:13 +0530

    Update dijkstra.cpp

--- Graph_algorithms/dijkstra.cpp
+++ Graph_algorithms/dijkstra.cpp
@@ -20,3 +20,33 @@ void dijkstra(int s){
   }
   
 }
+//prority_queue nlogn implementation of dijktras algorithm
+int n,m;
+ll d[mxN];
+vector<ar<ll,2>>adj[mxN];
+int main() {
+  cin >> n >> m ;
+  for(int i=0,a,b,c;i<m;i++){
+    cin >>a >> b >> c ;
+    adj[a].pb({c,b});
+  }
+  priority_queue<ar<ll,2>,vector<ar<ll,2>>,greater<ar<ll,2>>>pq ;
+  memset(d,0x3f,sizeof(d)) ;
+  d[1]=0 ;pq.push({0,1}) ;
+  while(pq.size()){
+    ar<ll,2>u=pq.top();
+    pq.pop();
+    if(u[0]>d[u[1]])
+      continue ;
+    for(ar<ll,2> v:adj[u[1]]){
+      if(d[v[1]]>u[0]+v[0]){
+        d[v[1]]=u[0]+v[0] ;
+        pq.push({d[v[1]],v[1]}) ;
+      }
+    }
+  }
+  For(i,n)
+    cout << d[i+1] << " ";
+	return 0;
+}
+
