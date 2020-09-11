a80fa1d54d945541c18757ed80f2a56f562e2227
Achintya777 <59294509+Achintya777@users.noreply.github.com>
2020-09-11 20:47:09 +0530

    Update diagraph.cpp

--- Graph_algorithms/diagraph.cpp
+++ Graph_algorithms/diagraph.cpp
@@ -1,36 +1,36 @@
-struct digraph {
-	int n;
-	vi<vi<int>> adj, adj2;
-	digraph(int _n, vi<ar<int, 2>> &e) {
-		n=_n;
-		adj=vi<vi<int>>(n);
-		adj2=vi<vi<int>>(n);
-		EACH(a, e) {
-			adj[a[0]].pb(a[1]);
-			adj2[a[1]].pb(a[0]);
-		}
-	}
-	vi<int> toposort() {
-		vi<int> d(n);
-		queue<int> qu;
-		FOR(n) {
-			d[i]=sz(adj2[i]);
-			if(!d[i])
-				qu.push(i);
-		}
-		vi<int> ans;
-		while(qu.size()) {
-			int u=qu.front();
-			qu.pop();
-			EACH(v, adj[u]) {
-				--d[v];
-				if(!d[v])
-					qu.push(v);
-			}
-			ans.pb(u);
-		}
-		if(sz(ans)<n)
-			ans.clear();
-		return ans;
-	}
+struct digraph{
+  int n ;
+  vector<vector<int>>adj,adj2 ;
+  digraph(int _n,vector<ar<int,2>> &e){
+    n=_n ;
+    adj=vector<vector<int>>(n);
+    adj2=vector<vector<int>>(n);
+    for(auto a:e){
+      adj[a[0]].push_back(a[1]);
+      adj2[a[1]].push_back(a[0]);
+    }
+  }
+  vector<int>toposort(){
+    queue<int>qu ;
+    vector<int>ans ;
+    vector<int>d(n) ;
+    for(int i=0;i<n;i++){
+      d[i]=adj2[i].size() ;
+      if(!d[i])
+        qu.push(i) ;
+    }
+    while(qu.size()){
+      int u=qu.front() ;
+      qu.pop() ;
+      for(int x:adj[u]){
+        --d[x] ;
+        if(!d[x])
+          qu.push(x) ;
+      }
+      ans.push_back(u) ;
+    }
+    if(ans.size()<n)
+      ans.clear() ;
+    return ans ;
+  }
 };
